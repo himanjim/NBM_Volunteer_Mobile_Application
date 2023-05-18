b2ab367d5c1ecba5f0ea2cc8249fa6c068bf2fb5
himanshu4529.pec@gmail.com <himanshu4529.pec@gmail.com>
2023-05-18 23:32:55 +0530

    Fragments and ViewPager2: tabs working

--- app/src/main/java/com/dot/nbm/MainActivity.java
+++ app/src/main/java/com/dot/nbm/MainActivity.java
@@ -1,34 +1,44 @@
 package com.dot.nbm;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.widget.TableLayout;
-
-import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] labels = new String[]{"QoS", "About Us", "Contributions", "Test"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TableLayout tableLayout = findViewById(R.id.tabLayout);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
 
-        TabItem qosTab = findViewById(R.id.qosTab);
-        TabItem aboutUsTab = findViewById(R.id.aboutUsTab);
-        TabItem contributionsTab = findViewById(R.id.contributionsTab);
-        TabItem bestOperatorTab = findViewById(R.id.bestOperatorTab);
+//        TabItem qosTab = findViewById(R.id.qosTab);
+//        TabItem aboutUsTab = findViewById(R.id.aboutUsTab);
+//        TabItem contributionsTab = findViewById(R.id.contributionsTab);
+//        TabItem bestOperatorTab = findViewById(R.id.bestOperatorTab);
 
         ViewPager2 viewPager2 = findViewById(R.id.viewPager);
 
-        PagerAdaptor pagerAdaptor = new PagerAdaptor(getSupportFragmentManager(), getLifecycle());
+        PagerAdaptor pagerAdaptor = new PagerAdaptor(this);
+//        pagerAdaptor.addFragment(new QoSFragment());
+//        pagerAdaptor.addFragment(new AboutFragment());
+//        pagerAdaptor.addFragment(new ContributionsFragment());
+//        pagerAdaptor.addFragment(new BestOperatorFragment());
         viewPager2.setAdapter(pagerAdaptor);
-    }
 
+//        getSupportActionBar().setElevation(0);
+
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+            tab.setText(labels[position]);
+        }).attach();
+// set default position to 1 instead of default 0
+        viewPager2.setCurrentItem(1, false);
+    }
 }
--- app/src/main/java/com/dot/nbm/PagerAdaptor.java
+++ app/src/main/java/com/dot/nbm/PagerAdaptor.java
@@ -2,34 +2,35 @@ package com.dot.nbm;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Lifecycle;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class PagerAdaptor extends FragmentStateAdapter {
-    public PagerAdaptor(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
-        super(fragmentManager, lifecycle);
+
+    public PagerAdaptor(FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        switch (position){
+        switch (position) {
             case 0:
-                return new QoSFragment();
+                return new QoSFragment(); // calls fragment
             case 1:
-                return new AboutFragment();
+                return new AboutFragment(); // chats fragment
             case 2:
-                return new ContributionsFragment();
+                return new ContributionsFragment(); // status fragment
             case 3:
                 return new BestOperatorFragment();
         }
-        return null;
+        return new QoSFragment(); //chats fragment
     }
+
 
     @Override
     public int getItemCount() {
-        return 4;
+        return 3;
     }
 }
